9f18c09de37f41b1fd2e1a02fd943a6a952e1a83
Kevin Gray <Kevin@GoPosse.com>
2019-06-07 15:52:19 -0400

    initial commit

---
+++ lib/listenable_builder.dart
@@ -0,0 +1,12 @@
+library listenable_builder;
+
+import 'package:flutter/material.dart';
+
+class ListenableBuilder extends AnimatedBuilder {
+  ListenableBuilder({
+    Key key,
+    @required Listenable listenable,
+    @required TransitionBuilder builder,
+    Widget child,
+  }) : super(key: key, animation: listenable, builder: builder, child: child);
+}
